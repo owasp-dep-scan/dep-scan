0e5c665c44997008292629243a894c20c0a926f1
Prabhu Subramanian <prabhu@shiftleft.io>
2022-12-18 02:34:40 +0000

    Tweaks

--- contrib/npm-watch.py
+++ contrib/npm-watch.py
@@ -1,22 +1,21 @@
 import json
-import requests
+import httpx
 
 from depscan.lib.pkg_query import npm_pkg_risk
 
 settings = {"interval": "1000", "count": "50", "include_docs": "true"}
 url = "https://replicate.npmjs.com/_changes"
 
-r = requests.get(url, params=settings, stream=True, timeout=30)
-
-for line in r.iter_lines():
-    if line:
-        line = line.decode("utf-8")[:-1]
-        try:
-            json_obj = json.loads(line)
-            npm_pkg = json_obj.get("id")
-            risk_metrics = npm_pkg_risk(json_obj.get("doc"), False, None)
-            if risk_metrics and risk_metrics["risk_score"] > 0.6:
-                print(npm_pkg, risk_metrics)
-        except Exception as e:
-            print(line, e)
-            pass
+with httpx.stream("GET", url=url, params=settings, timeout=30) as r:
+    for line in r.iter_lines():
+        if line:
+            line = line[:-2]
+            try:
+                json_obj = json.loads(line)
+                npm_pkg = json_obj.get("id")
+                risk_metrics = npm_pkg_risk(json_obj.get("doc"), False, None)
+                if risk_metrics and risk_metrics["risk_score"] > 0.4:
+                    print(npm_pkg, risk_metrics)
+            except Exception as e:
+                print(line, e)
+                pass
