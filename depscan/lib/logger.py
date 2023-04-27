dbfd31e75bbb09e2ed83059692198e9afe57db26
Prabhu Subramanian <prabhu@appthreat.com>
2023-04-27 17:23:15 +0000

    Use latest vdb with fix for msgpack
    Signed-off-by: Prabhu Subramanian <prabhu@appthreat.com>

--- depscan/lib/logger.py
+++ depscan/lib/logger.py
@@ -42,6 +42,8 @@ logging.basicConfig(
     ],
 )
 LOG = logging.getLogger(__name__)
+for _ in ("httpx",):
+    logging.getLogger(_).disabled = True
 
 # Set logging level
 if os.getenv("SCAN_DEBUG_MODE") == "debug":
