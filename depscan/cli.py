d444d5a0c2f813cd66c91a584da0d7fbaa30389f
Florian Heubeck <heubeck@mediamarktsaturn.com>
2023-12-21 18:58:43 +0100

    Declare variables required for report
    Sometimes there are
    ```
    Traceback (most recent call last):
      File "/usr/local/bin/depscan", line 8, in <module>
        sys.exit(main())
      File "/usr/local/lib/python3.9/site-packages/depscan/cli.py", line 1118, in main
        vdr_file=vdr_file,
    UnboundLocalError: local variable 'vdr_file' referenced before assignment
    ```
    errors on depscan executions, thus declaring all variables used for the
    reporting that are initialized only conditionally upfront.
    
    Signed-off-by: Florian Heubeck <heubeck@mediamarktsaturn.com>

--- depscan/cli.py
+++ depscan/cli.py
@@ -869,6 +869,7 @@ def main():
                 expand=False,
             )
         )
+    summary, vdr_file, bom_file  = None, None, None
     for project_type in project_types_list:
         results = []
         report_file = areport_file.replace(".json", f"-{project_type}.json")
