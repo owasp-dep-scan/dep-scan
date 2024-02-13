63c4fedea720d4a46f8e9108e6ce1ad2addeab43
Prabhu Subramanian <prabhu@appthreat.com>
2024-02-13 17:45:14 +0000

    Review comments
    Signed-off-by: Prabhu Subramanian <prabhu@appthreat.com>

--- depscan/lib/csaf.py
+++ depscan/lib/csaf.py
@@ -1313,12 +1313,12 @@ def parse_cvss(ratings):
     """
     if not ratings or not (vector_string := ratings[0].get("vector")):
         return {}
-    if not vector_string or vector_string == "None":
+    if vector_string == "None":
         return {}
     try:
         cvss_v3 = cvss.CVSS3(vector_string)
         cvss_v3.check_mandatory()
-    except [CVSSError, ValueError]:
+    except (CVSSError, ValueError):
         return {}
 
     cvss_v3_dict = cvss_v3.as_json()
