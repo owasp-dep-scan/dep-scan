f289935c2d8963c03907a2fa8f3dddc383910f7f
Aryan Rajoria <aryanrajoria1003@gmail.com>
2024-07-14 21:04:56 -0400

    add risk_metrics

--- depscan/package_query/cargo_pkg.py
+++ depscan/package_query/cargo_pkg.py
@@ -4,8 +4,22 @@ from depscan.lib import config
 from depscan.package_query.pkg_query import compute_time_risks, calculate_risk_score
 
 
-def cargo_pkg_risk():
+def cargo_pkg_risk(pkg_metadata, is_private_pk, scope, pkg):
     """
     Calculate various package risks based on the metadata from cargo.
     """
-    pass
+    risk_metrics = {
+        "pkg_deprecated_risk": False,
+        "pkg_version_deprecated_risk": False,
+        "pkg_version_missing_risk": False,
+        "pkg_includes_binary_risk": False,
+        "pkg_min_versions_risk": False,
+        "created_now_quarantine_seconds_risk": False,
+        "latest_now_max_seconds_risk": False,
+        "mod_create_min_seconds_risk": False,
+        "pkg_min_maintainers_risk": False,
+        "pkg_node_version_risk": False,
+        "pkg_private_on_public_registry_risk": False,
+    }
+
+    print(risk_metrics)
