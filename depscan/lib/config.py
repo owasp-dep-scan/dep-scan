0952168c3c8fd3a31f48c237a3373e1cee93fe2f
Prabhu Subramanian <prabhu@appthreat.com>
2025-12-18 00:00:12 +0000

    Fixes #461
    Signed-off-by: Prabhu Subramanian <prabhu@appthreat.com>

--- depscan/lib/config.py
+++ depscan/lib/config.py
@@ -117,7 +117,7 @@ def get_int_from_env(name, default):
 
 
 NPM_SERVER = "https://registry.npmjs.org"
-npm_app_info = {"name": "owasp-depscan", "version": "6.0.0"}
+npm_app_info = {"name": "owasp-depscan", "version": "6.1.0"}
 
 PYPI_SERVER = "https://pypi.org/pypi"
 
